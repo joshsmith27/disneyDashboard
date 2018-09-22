45042bda93d9fb8ae45905144c1909b48c16d5d3
Joshua Smith <veryjoshua@gmail.com>
2018-09-21 23:14:19 -0400

    finishing stuff

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ require('dotenv').config
 
 app.use(express.static(path.join(__dirname, './build')));
 
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/build/index.html'));
+});
 const port = process.env.PORT || 8007
 
 app.listen(port, () => {
